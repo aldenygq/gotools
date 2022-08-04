2f1e83d2a060d7e8dca234683dc63da59ef4c3e9
mac <mac@macdeMac-mini.local>
2022-08-04 11:14:34 +0800

    Initial commit

--- net/net.go
+++ net/net.go
@@ -4,6 +4,7 @@ import (
 	"net"
 )
 
+//校验ip是否合法
 func CheckIp(str string) bool {
 	result := net.ParseIP(str)
 	if result == nil {
@@ -13,3 +14,14 @@ func CheckIp(str string) bool {
 	return true
 }
 
+//获取本地ip
+func GetLocalIp() string {
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
+	if conn == nil {
+		return ""
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().String()
+	idx := strings.LastIndex(localAddr, ":")
+	return localAddr[0:idx]
+}
